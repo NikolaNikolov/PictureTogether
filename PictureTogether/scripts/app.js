e70849e9583dd0de78b97dbcafba92486bd401dd
Nikola Nikolov <nikola.antonov.nikolov@gmail.com>
2013-10-10 14:20:47 -0500

    Changes made by Nikola Nikolov (nikola.antonov.nikolov@gmail.com).

--- PictureTogether/scripts/app.js
+++ PictureTogether/scripts/app.js
@@ -1,9 +1,12 @@
 (function (global) {
     var app = global.app = global.app || {};
-
+    var storage = window.localStorage;
+    
     document.addEventListener("deviceready", function () {
         app.application = new kendo.mobile.Application(document.body, {
             layout: "tabstrip-layout"
         });
-    }, false);
+        
+        storage.setItem("sessionsKey", "pesho");
+    }, false);    
 })(window);
--- PictureTogether/scripts/login.js
+++ PictureTogether/scripts/login.js
@@ -1,20 +1,34 @@
 (function (global) {
     var LoginViewModel,
-        app = global.app = global.app || {};
+    app = global.app = global.app || {};
+    var storage = window.localStorage;
 
     LoginViewModel = kendo.data.ObservableObject.extend({
         isLoggedIn: false,
         username: "",
         password: "",
 
+        init: function () {
+            var that = this;
+            
+            kendo.data.ObservableObject.fn.init.apply(that, []);
+            
+            //that.isLoggedIn = true;
+            
+            if (storage.getItem("sessionsKey")) {
+                that.isLoggedIn = true;
+            }
+        },
+        
         onLogin: function () {
             var that = this,
-                username = that.get("username").trim(),
-                password = that.get("password").trim();
+            username = that.get("username").trim(),
+            password = that.get("password").trim();
 
             if (username === "" || password === "") {
                 navigator.notification.alert("Both fields are required!",
-                    function () { }, "Login failed", 'OK');
+                                             function () {
+                                             }, "Login failed", 'OK');
 
                 return;
             }
@@ -27,6 +41,7 @@
 
             that.clearForm();
             that.set("isLoggedIn", false);
+            storage.clear();
         },
 
         clearForm: function () {
